a499a3d0f789dc979f7cd4780de6912995f9961c
nasehqusyairy <nasyikhqusyairy@gmail.com>
2026-09-15 14:26:18 +0700

    feat: remove comments from sitemap XML generation for cleaner code

--- app/routes/seo/sitemap.ts
+++ app/routes/seo/sitemap.ts
@@ -52,13 +52,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
     // 6. Rakit string XML
     const content = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <!-- Halaman Utama / Root -->
   <url>
     <loc>${SEO_CONFIG.domain}/</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <priority>1.0</priority>
   </url>
-  <!-- Seluruh Rute Statis & Dinamis -->
   ${publicPaths
             .map(
                 (path) => `
